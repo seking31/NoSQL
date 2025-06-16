8cecc0949b489d9c76d312aedcbfa02f2e90be49
Sara King <sara@sara-king.com>
2025-06-15 19:12:18 -0500

    week_2

---
+++ week_2/exercise2-3.py
@@ -0,0 +1,12 @@
+"""
+Author: Sara King
+Date: June 15, 2025
+File Name: quote_printer.py
+Description: This file demonstrates the use of both file header comments and
+normal level comments. It prints a motivational quote along with the author's name.
+"""
+
+def quote():
+     print(f'"We gladly feast on those who would subdue us" — The Addams Family')
+
+quote()
